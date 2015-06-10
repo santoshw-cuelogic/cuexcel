9c0fab94fa9bc1d770c4b81feceb56820855aa51
= <=>
2015-06-10 17:05:17 +0530

    #cuexcel asset precompilation removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Cuexcel::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
-  config.precompile_assets = true
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
